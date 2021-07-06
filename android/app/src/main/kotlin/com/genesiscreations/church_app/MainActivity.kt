d1cd3089fcb0903b08e735d367460e0775983c2e
assemelmansifm <assem.elmansi@outlook.com>
2021-07-06 17:22:14 +0200

    Fonts and Colors init

---
+++ android/app/src/main/kotlin/com/genesiscreations/church_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.genesiscreations.church_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/AppColor.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColor {
+  static const PrimaryColor = Color(0xFF005a8c);
+  static const SecondaryColor = Color(0xFFd7545a);
+}
