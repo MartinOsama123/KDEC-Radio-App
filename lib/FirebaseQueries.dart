17c6c5ee0501b5ef33787cd55946a3443f0e8051
Martin Osama <martinosama2015@gmail.com>
2021-07-17 17:30:57 +0200

    Added Queue System

--- lib/FirebaseQueries.dart
+++ lib/FirebaseQueries.dart
@@ -14,13 +14,24 @@ class FirebaseQueries {
   }
   static Future<List<MediaItem>> getAlbumPlaylist(String albumName) async {
     firebase_storage.ListResult result = await firebase_storage.FirebaseStorage.instance.ref().child(albumName).listAll();
-    List<MediaItem> list =  <MediaItem>[];
+
     AudioPlayer audioPlayer = new AudioPlayer();
     for(firebase_storage.Reference r in result.items.toList()){
       String download = await r.getDownloadURL();
       await audioPlayer.setUrl(download);
-      list.add(new MediaItem(id: download, album: albumName, title: r.name,duration: audioPlayer.duration ?? Duration()));
+      QueueSystem.add(new  MediaItem(id: download, album: albumName, title: r.name,duration: audioPlayer.duration ?? Duration()));
     }
-    return list;
+    return QueueSystem.getQueue;
   }
+}
+class QueueSystem {
+   static  List<MediaItem> _queue =  <MediaItem>[];
+   static void add(MediaItem item) => _queue.add(item);
+   static MediaItem getItem(index) => _queue[index];
+   static bool isLast(int index) => index == _queue.length-1;
+   static bool isFirst(int index) => index == 0;
+   static void clearQueue() => _queue.clear();
+   static void isEmpty() => _queue.isEmpty;
+   static void isNotEmpty() => _queue.isNotEmpty;
+   static get getQueue => _queue;
 }
