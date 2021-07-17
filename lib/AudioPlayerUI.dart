aa1df09c9f5956f02e7178039b736be6491c905a
Martin Osama <martinosama2015@gmail.com>
2021-07-17 16:40:52 +0200

    AudioPlayerUI Colorization and Modifications

--- lib/AudioPlayerUI.dart
+++ lib/AudioPlayerUI.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:audio_service/audio_service.dart';
+import 'package:church_app/AppColor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,9 @@ class AudioPlayerUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+
+      appBar: AppBar(leading: IconButton(onPressed: ()=>Navigator.pop(context),icon: Icon(Icons.arrow_back),color: AppColor.PrimaryColor),backgroundColor: Colors.white,elevation: 0,),
         body: SafeArea(
           child: StreamBuilder<MediaItem?>(
             stream: AudioService.currentMediaItemStream,
@@ -17,15 +21,20 @@ class AudioPlayerUI extends StatelessWidget {
              Expanded(
                 child: Image.asset('images/logo.jpg')),
             AudioSlider(duration: mediaItem.data?.duration ?? Duration()),
-            Center(
-              child: Row(children: [
-                PlayButton(),
-                IconButton(onPressed: () {}, icon: Icon(Icons.play_arrow)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.play_arrow)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.play_arrow)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.play_arrow)),
-              ]),
-            ),
+     Padding(
+       padding: const EdgeInsets.all(8.0),
+       child: Row(
+         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+         crossAxisAlignment: CrossAxisAlignment.center,
+           children: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.repeat)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.skip_previous)),
+                  PlayButton(),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.skip_next)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.shuffle)),
+                ]),
+     ),
+
       ],
     ) : CircularProgressIndicator(),
           ),
@@ -52,9 +61,14 @@ class _PlayButtonState extends State<PlayButton> with TickerProviderStateMixin  
       stream: AudioService.playbackStateStream,
       builder: (context, snapshot) {
         if(!snapshot.data!.playing) iconController.forward(); else iconController.reverse();
-        return IconButton(
-        onPressed: () {snapshot.data!.playing ? AudioService.pause() : AudioService.play();},
-        icon:  AnimatedIcon(icon: AnimatedIcons.pause_play, progress:iconController) ,iconSize: 40,);}
+        return CircleAvatar(
+          backgroundColor: AppColor.SecondaryColor,
+          radius: 30,
+          child: IconButton(
+            color: Colors.white,
+          onPressed: () {snapshot.data!.playing ? AudioService.pause() : AudioService.play();},
+          icon:  AnimatedIcon(icon: AnimatedIcons.pause_play, progress:iconController) ,iconSize: 35,),
+        );}
     );
   }
 }
@@ -71,6 +85,7 @@ class AudioSlider extends StatelessWidget {
         builder: (context, snapshot) => Column(
           children: [
             Slider(
+              activeColor: AppColor.PrimaryColor,
                 value: snapshot.data?.inSeconds?.toDouble() ?? 0.0,
                 min: 0,
                 max:  duration?.inSeconds.toDouble() ?? 499 ,
