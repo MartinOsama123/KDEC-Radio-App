d42092d0cccc96af04961d12a70f9e1ddfcc2d79
Martin Osama <martinosama2015@gmail.com>
2021-08-27 09:51:25 +0200

    Added search screen,Social icons adjustment, new chat floating icon & album screen subscription button with album cover adjustments

--- lib/Screens/AlbumScreen.dart
+++ lib/Screens/AlbumScreen.dart
@@ -4,6 +4,7 @@ import 'package:church_app/main.dart';
 import 'package:church_app/models/AlbumInfo.dart';
 import 'package:flutter/material.dart';
 import 'package:social_share/social_share.dart';
+
 class AlbumScreen extends StatelessWidget {
   final AlbumInfo albumInfo;
 
@@ -16,7 +17,10 @@ class AlbumScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         leading: IconButton(
-            icon: Icon(Icons.arrow_back,color: AppColor.PrimaryColor,),
+            icon: Icon(
+              Icons.arrow_back,
+              color: AppColor.PrimaryColor,
+            ),
             onPressed: () => Navigator.pop(context)),
         elevation: 0,
         backgroundColor: Colors.white,
@@ -29,7 +33,11 @@ class AlbumScreen extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 5),
-                child: Hero(tag: albumInfo.albumName,child: Image.network("https://kdechurch.herokuapp.com/api/img/${albumInfo.imgPath}")),
+                child: Hero(
+                    tag: albumInfo.albumName,
+                    child: Image.network(
+                        "https://kdechurch.herokuapp.com/api/img/${albumInfo.imgPath}",
+                        height: MediaQuery.of(context).size.height / 2)),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(vertical: 4),
@@ -45,44 +53,98 @@ class AlbumScreen extends StatelessWidget {
                         context: context,
                         builder: (BuildContext context) {
                           return Scaffold(
-                              backgroundColor: Colors.transparent,
-                              body: Center(
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Container(
+                            backgroundColor: Colors.transparent,
+                            body: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  children: [
+                                    const Text("Share to"),
+                                    Container(
                                       height: 120,
-                                      decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.all(Radius.circular(15))),
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(15))),
                                       child: SingleChildScrollView(
                                         scrollDirection: Axis.horizontal,
                                         child: Row(
                                           children: [
-                                            InkWell(child: Image.asset("images/facebook-logo.png"),onTap: (){}),
-                                            InkWell(child: Image.asset("images/instagram-logo.png"),onTap: ()async {
-
-                                              await SocialShare.shareInstagramStory("https://kdechurch.herokuapp.com/api/img/${albumInfo.imgPath}",attributionURL: "Listen to ${albumInfo.albumName} on http://kdec.com/",backgroundImagePath: "https://kdechurch.herokuapp.com/api/img/${albumInfo.imgPath}");}),
-                                            InkWell(child: Image.asset("images/whatsapp-logo.png"),onTap: () async { await SocialShare.shareWhatsapp("Listen to ${albumInfo.albumName} on http://kdec.com/");}),
-                                            InkWell(child: Image.asset("images/twitter-logo.png"),onTap: (){SocialShare.shareTwitter(
-                                                "Listen to this new album",
-                                                hashtags: ["kdec", "album", "fun", "hi"],url:"https://kdec.com/");}),
+                                            InkWell(
+                                                child: Image.asset(
+                                                  "images/facebook-logo.png",
+                                                  height: 32,
+                                                  width: 32,
+                                                ),
+                                                onTap: () {}),
+                                            InkWell(
+                                                child: Image.asset(
+                                                  "images/instagram-logo.png",
+                                                  height: 32,
+                                                  width: 32,
+                                                ),
+                                                onTap: () async {
+                                                  await SocialShare.shareInstagramStory(
+                                                      "https://kdechurch.herokuapp.com/api/img/${albumInfo.imgPath}",
+                                                      attributionURL:
+                                                          "Listen to ${albumInfo.albumName} on http://kdec.com/",
+                                                      backgroundImagePath:
+                                                          "https://kdechurch.herokuapp.com/api/img/${albumInfo.imgPath}");
+                                                }),
+                                            InkWell(
+                                                child: Image.asset(
+                                                  "images/whatsapp-logo.png",
+                                                  height: 32,
+                                                  width: 32,
+                                                ),
+                                                onTap: () async {
+                                                  await SocialShare.shareWhatsapp(
+                                                      "Listen to ${albumInfo.albumName} on http://kdec.com/");
+                                                }),
+                                            InkWell(
+                                                child: Image.asset(
+                                                  "images/twitter-logo.png",
+                                                  height: 32,
+                                                  width: 32,
+                                                ),
+                                                onTap: () {
+                                                  SocialShare.shareTwitter(
+                                                      "Listen to this new album ",
+                                                      hashtags: [
+                                                        "kdec",
+                                                        "album",
+                                                        "fun",
+                                                        "hi"
+                                                      ],
+                                                      url: "https://kdec.com/");
+                                                }),
                                           ],
                                         ),
                                       ),
                                     ),
-                                  ),
+                                  ],
                                 ),
-
-                            );
+                              ),
+                            ),
+                          );
                         },
                       );
-
                     },
                     style: ElevatedButton.styleFrom(
                       onPrimary: Colors.white,
                       primary: AppColor.SecondaryColor,
                     ),
-                    icon: Icon(Icons.bookmark_border),
+                    icon: Icon(Icons.share),
                     label: const Text("Share")),
               ),
+              ElevatedButton.icon(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    onPrimary: Colors.white,
+                    primary: AppColor.SecondaryColor,
+                  ),
+                  icon: Icon(Icons.bookmark_border),
+                  label: const Text("Subscribe")),
               PlaylistWidget(albumName: albumInfo.albumName),
             ],
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -20,19 +20,26 @@ import 'package:provider/provider.dart';
 import 'Screens/NotificationScreen.dart';
 import 'Screens/SplashScreen.dart';
 
-const AndroidNotificationChannel channel = AndroidNotificationChannel("channel", "title", "Description"
-,importance: Importance.high,playSound: true);
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async{
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    "channel", "title", "Description",
+    importance: Importance.high, playSound: true);
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
   print("a bg message just showed up ${message.messageId}");
 }
-Future<void> main() async  {
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  await flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()?.createNotificationChannel(channel);
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(alert: true,badge: true,sound: true);
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+      alert: true, badge: true, sound: true);
   await FirebaseMessaging.instance.subscribeToTopic("Global");
 
   runApp(MyApp());
@@ -42,36 +49,39 @@ class MyApp extends StatelessWidget {
   final language = false;
   @override
   Widget build(BuildContext context) {
-    return  MultiProvider(
-        providers: [
+    return MultiProvider(
+      providers: [
         Provider<FirebaseAuthService>(
-        create: (_) => FirebaseAuthService(FirebaseAuth.instance),
-    ),
-    StreamProvider(
-    create: (context) => context.read<FirebaseAuthService>().authStateChanges, initialData: null,
-    )
-    ],
+          create: (_) => FirebaseAuthService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) =>
+              context.read<FirebaseAuthService>().authStateChanges,
+          initialData: null,
+        )
+      ],
       child: MaterialApp(
         onGenerateRoute: (settings) {
-      // Handle '/'
-      if (settings.name == '/') {
-        return MaterialPageRoute(builder: (context) => MyHomePage());
-      }
+          // Handle '/'
+          if (settings.name == '/') {
+            return MaterialPageRoute(builder: (context) => MyHomePage());
+          }
 
-      // Handle '/details/:id'
-      var uri = Uri.parse(settings.name ?? "");
-      if (uri.pathSegments.first == 'album') {
-        var id = uri.pathSegments[1];
-        var path = uri.pathSegments[2];
-        return MaterialPageRoute(builder: (context) =>
-            AlbumScreen(
-                albumInfo: new AlbumInfo(albumName: id, imgPath: path)));
-      }},
+          // Handle '/details/:id'
+          var uri = Uri.parse(settings.name ?? "");
+          if (uri.pathSegments.first == 'album') {
+            var id = uri.pathSegments[1];
+            var path = uri.pathSegments[2];
+            return MaterialPageRoute(
+                builder: (context) => AlbumScreen(
+                    albumInfo: new AlbumInfo(albumName: id, imgPath: path)));
+          }
+        },
         theme: ThemeData(
             primaryColor: AppColor.PrimaryColor,
             accentColor: AppColor.SecondaryColor,
             fontFamily: language ? 'GESSTwo' : 'ABEAKRG'),
-        home:  SplashScreen(),
+        home: SplashScreen(),
       ),
     );
   }
@@ -88,19 +98,32 @@ class _MyHomePageState extends State<MyHomePage> {
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
-      _selectedName = index == 0 ? "Library" : index == 1 ? "Browse" : "Notification";
+      _selectedName = index == 0
+          ? "Library"
+          : index == 1
+              ? "Browse"
+              : "Notification";
     });
   }
+
   @override
   void initState() {
-
     connectAudio();
-    FirebaseMessaging.onMessage.listen((event) {RemoteNotification? notification = event.notification;
-    AndroidNotification? androidNotification = event.notification?.android;
-    if(notification != null && androidNotification != null){
-      flutterLocalNotificationsPlugin.show(notification.hashCode, notification.title, notification.body,
-          NotificationDetails(android: AndroidNotificationDetails(channel.id,channel.name,channel.description,color: Colors.black,playSound: true,icon: "@mipmap/ic_launcher")));
-    }
+    FirebaseMessaging.onMessage.listen((event) {
+      RemoteNotification? notification = event.notification;
+      AndroidNotification? androidNotification = event.notification?.android;
+      if (notification != null && androidNotification != null) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+                android: AndroidNotificationDetails(
+                    channel.id, channel.name, channel.description,
+                    color: Colors.black,
+                    playSound: true,
+                    icon: "@mipmap/ic_launcher")));
+      }
     });
 
     super.initState();
@@ -119,48 +142,59 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(title:   Text(
-         _selectedName,
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.black,
-          )),elevation: 0,backgroundColor: Colors.transparent,actions: [CircleAvatar(backgroundColor: Colors.grey,child: IconButton(onPressed: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
-      }, icon: Icon(Icons.search))),Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: CircleAvatar(backgroundColor: AppColor.SecondaryColor,child: IconButton(onPressed: (){}, icon: Icon(Icons.person))),
-          ),]),
-      body: SafeArea(
-          child: Scaffold(
-            floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-            floatingActionButton: FloatingContainer(),
-            body: Center(
-                  child: _buildScreens().elementAt(_selectedIndex),
-                ),
-
-            bottomNavigationBar: BottomNavigationBar(
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.circle_fill),
-                  label: ("Library"),
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.square_list),
-                  label: ("Browse"),
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.bell),
-                  label: ("Notifications"),
-                ),
-              ],
-              currentIndex: _selectedIndex,
-              selectedItemColor: AppColor.PrimaryColor,
-              unselectedItemColor: Colors.grey,
-
-              onTap: _onItemTapped,
+      appBar: AppBar(
+          title: Text(_selectedName,
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.black,
+              )),
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                  backgroundColor: AppColor.SecondaryColor,
+                  child: IconButton(
+                      onPressed: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoginScreen())),
+                      icon: Icon(Icons.person))),
             ),
+          ]),
+      body: SafeArea(
+        child: Scaffold(
+
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerFloat,
+          floatingActionButton: FloatingContainer(),
+          body: Center(
+            child: _buildScreens().elementAt(_selectedIndex),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.circle_fill),
+                label: ("Library"),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.square_list),
+                label: ("Browse"),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.bell),
+                label: ("Notifications"),
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: AppColor.PrimaryColor,
+            unselectedItemColor: Colors.grey,
+            onTap: _onItemTapped,
           ),
         ),
+      ),
     );
   }
 
@@ -180,44 +214,55 @@ class FloatingContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<MediaItem?>(
-                    stream: AudioService.currentMediaItemStream,
-                    builder: (context, mediaSnap) => mediaSnap.hasData
-                        ? GestureDetector(
-                      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => AudioPlayerUI(songName: mediaSnap.data?.title ?? ""))),
+    return Row(
+      children: [
+        CircleAvatar(child: IconButton(onPressed: (){},icon: Icon(Icons.message))),
+        StreamBuilder<MediaItem?>(
+            stream: AudioService.currentMediaItemStream,
+            builder: (context, mediaSnap) => mediaSnap.hasData
+                ? GestureDetector(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AudioPlayerUI(
+                                songName: mediaSnap.data?.title ?? ""))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width - 20,
+                          decoration: BoxDecoration(
+                              color: AppColor.SecondaryColor,
+                              borderRadius: BorderRadius.circular(8)),
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                                height: 50,
-                                width: MediaQuery.of(context).size.width - 20,
-                                decoration: BoxDecoration(
-                                    color: AppColor.SecondaryColor,
-                                    borderRadius: BorderRadius.circular(8)),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                      mainAxisSize: MainAxisSize.min,
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: [
-                                    Column(
-                                      children: [
-                                        Text(mediaSnap.data?.title ?? "",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.w400,
-                                                fontSize: 16)),
-                                        Text(mediaSnap.data?.album ?? "",
-                                            style: TextStyle(
-                                                fontWeight: FontWeight.w400,
-                                                fontSize: 10)),
-                                      ],
-                                    ),
-                                        Spacer(),
-                                        PlayButton(radius: 15,iconSize: 15),
-                                        IconButton(onPressed: (){}, icon: Icon(Icons.stop))
-                                  ]),
-                                )),
-                          ),
-                        )
-                        : Container());
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Text(mediaSnap.data?.title ?? "",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 16)),
+                                      Text(mediaSnap.data?.album ?? "",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 10)),
+                                    ],
+                                  ),
+                                  Spacer(),
+                                  PlayButton(radius: 15, iconSize: 15),
+                                  IconButton(
+                                      onPressed: () {}, icon: Icon(Icons.stop))
+                                ]),
+                          )),
+                    ),
+                  )
+                : Container()),
+      ],
+    );
   }
 }
